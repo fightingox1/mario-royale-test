f26e94d7bc30a4cbfb934cd640f47d0055094074
fightingox1 <44482532+fightingox1@users.noreply.github.com>
2019-12-12 11:19:58 -0500

    Update server.js

--- js/server.js
+++ js/server.js
@@ -1,3 +1,3 @@
 //var WEBSOCKET_SERVER = "ws://127.0.0.1:9000/royale/ws";
-var WEBSOCKET_SERVER = "ws://marioroyale.cyuubi.gq:9000/royale/ws";
+var WEBSOCKET_SERVER = "ws://fightingox1.github.io/mario-royale-personal-server:9000/royale/ws";
 print("loading server.js finished");
